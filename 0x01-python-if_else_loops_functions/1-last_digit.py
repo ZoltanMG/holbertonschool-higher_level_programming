2300068db8aef6fd163cad3dd568ade93dbd7b47
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-09-01 17:17:16 +0000

    [add]: refacture

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,10 @@ if number < 0:
     last_number = (number * -1) % 10
 else:
     last_number = number % 10
-print('Last digit of {} is {} and is '.format(number, last_number), end='')
+print('Last digit of {} is {} '.format(number, last_number), end='')
 if last_number > 5:
-    print('greater than 5')
+    print('and is greater than 5')
 elif last_number == 0:
-    print('0')
+    print('and is 0')
 else:
-    print('less than 6 and not 0')
+    print('and is less than 6 and not 0')
