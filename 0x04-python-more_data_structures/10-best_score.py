83c7b7f1a6e506fd9014caba91ad7f506fe1bee7
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-09-10 23:23:21 +0000

    [update]: refacture

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    score = 0
     name = ''
 
     if a_dictionary is None:
         return None
 
-    for i, j in sorted(a_dictionary.items()):
-        name = i
+    for i, j in a_dictionary.items():
+        if j > score:
+            name = i
+            score = j
     return name
