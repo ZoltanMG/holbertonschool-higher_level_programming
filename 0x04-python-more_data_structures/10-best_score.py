d3a3ed9141b660ef1e51380234a5032c880140a1
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-09-10 22:15:04 +0000

    [update]: add egde case

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    score = -1000
+    score = 0
 
-    if a_dictionary:
+    if a_dictionary is not None:
         for i, j in a_dictionary.items():
             if j > score:
                 score = j
