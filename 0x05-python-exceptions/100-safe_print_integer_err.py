bb2ad9504a8a3b62de44a0c5674dceaf57afd5bd
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-09-21 22:36:08 +0000

    [add]: function that prints an integer.

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError as v_err:
+        sys.stderr.write("Exception: {}\n".format(v_err))
+        return False
+    except TypeError as t_err:
+        sys.stderr.write("Exception: {}\n".format(T_err))
+        return False
+
+    return True
