63959b5766e6ddf4340f3dec5c573e65451b083a
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-09-21 18:12:53 +0000

    [Update]: Refacture

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
         for i in range(x):
-            print('{}'.format(my_list[i], end=''))
+            print('{}'.format(my_list[i]), end='')
+            count += 1
+
+        print()
     except IndexError:
         print()
-        return i
-    print()
-    return i + 1
+    return count
