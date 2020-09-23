6d23c9b705c0aafc2f3920431eacfe65066c6abc
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-09-23 02:19:04 +0000

    [add]: class Square that defines a square by: (based on 1-square.py)

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""  class Square that defines a square by: (based on 1-square.py) """
+
+
+class Square:
+    """  class Square that defines a square by: (based on 1-square.py)"""
+
+    def __init__(self, size=0):
+        """ stored size if size is a integer >= 0"""
+
+        try:
+            if type(size) == int:
+                if size >= 0:
+                    self.__size = size
+                else:
+                    raise ValueError('size must be >= 0')
+            else:
+                raise TypeError('size must be an integer')
+        except (TypeError, ValueError):
+            raise
