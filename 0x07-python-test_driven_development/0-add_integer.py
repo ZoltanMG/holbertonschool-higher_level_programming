0ea099219eca59f4009644821dbfef24c6b46ff5
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-09-25 20:06:35 +0000

    [update]: add documentation and valide pep8.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+Function that adds two integers
+"""
+
+
 def add_integer(a, b=98):
+    """ Function that adds two integers """
+
     if a is not None:
         if type(a) not in (float, int):
             raise TypeError('a must be an integer')
