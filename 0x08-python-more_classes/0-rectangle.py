b6712cb7a058802a5a73a051636b39838d2a0e74
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-09-28 18:48:17 +0000

    [update]: refacture.

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -3,4 +3,5 @@
 
 
 class Rectangle:
+    """ this is a class empty"""
     pass
--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,36 +1,26 @@
 #!/usr/bin/python3
-""" an empty class Rectangle that defines a rectangle: """
+"""
+In this Python script, a class called rectangle is created,
+where width and height attributes are declared and initialized.
+"""
 
 
 class Rectangle:
+    """
+    in this class is declared
+    the width and height attributes
+    """
     def __init__(self, width=0, height=0):
-        """ inicializa los valores de width y height """
-
-        if type(width) != int:
-            raise TypeError('width must be an integer')
-        if width < 0:
-            raise ValueError('width must be >= 0')
-        if type(height) != int:
-            raise TypeError('height must be an integer')
-        if height < 0:
-            raise ValueError('height must be >= 0')
-
-        self.wigth = width
-        self.height = height
-        # como prueba no encapsulé las variables en el init
-        # pero si en los setters and getters
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
-        """ retorna el valor de width """
-
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ actualiza o cambia el valor de width """
-
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
@@ -38,15 +28,11 @@ class Rectangle:
 
     @property
     def height(self):
-        """ retorna el valor de height """
-
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ actualiza o cambia el valor de hetght"""
-
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
