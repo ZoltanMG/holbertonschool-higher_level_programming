b06b889c5f3b0444f3805f17da07d13acaee43fd
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-09-29 00:35:07 +0000

    [update]: changed conditionals

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
     def width(self, value):
         """ actualiza el ancho """
 
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
@@ -43,7 +43,7 @@ class Rectangle:
     def height(self, value):
         """ cambia el alto """
 
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
