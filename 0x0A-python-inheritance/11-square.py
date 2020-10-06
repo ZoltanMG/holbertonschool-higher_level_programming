872c491b9fcf1e7ffead59879cecdfbb5187de67
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-10-06 18:20:44 -0500

    [Add]: inheritance.

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" create a square class """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ my square """
+
+    def __init__(self, size):
+        """ init method"""
+
+        self.__size = size
+        self.integer_validator('size', self.__size)
+        super().__init__(size, size)
+
+    def area(self):
+        """ area """
+
+        return super().area()
+
+    def __str__(self):
+        """ magic method"""
+
+        return '[Square] {}/{}'.format(self.__size, self.__size)
