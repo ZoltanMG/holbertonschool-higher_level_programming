cb00e30896a559ff71168168459f4c6b6d66f4ae
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-10-06 21:01:36 -0500

    [Add]: Refacture.

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,5 +13,5 @@ class Rectangle(BaseGeometry):
 
         self.__width = width
         self.__height = height
-        BaseGeometry.integer_validator(self, "width", self.__width)
-        BaseGeometry.integer_validator(self, "height", self.__height)
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
