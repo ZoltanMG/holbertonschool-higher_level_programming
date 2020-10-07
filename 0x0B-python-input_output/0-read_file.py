a7da5720d347eb3a1fb7b11911bf8b5eb4167839
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-10-07 23:00:45 +0000

    [Update]: changed string to variables.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 def read_file(filename=""):
     """ open file"""
 
-    with open('my_file_0.txt', encoding='utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         for line in f:
             print(line, end='')
--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -6,7 +6,7 @@ def number_of_lines(filename=""):
     """ open file"""
 
     number_lines = 0
-    with open('my_file_0.txt', encoding='utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         for line in f:
             number_lines += 1
     return number_lines
