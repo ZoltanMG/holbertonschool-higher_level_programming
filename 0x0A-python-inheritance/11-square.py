118ba53523ab71d75e0b7b394a940ed7e9373edf
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-10-06 21:08:03 -0500

    [Update]: refacture

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -11,8 +11,8 @@ class Square(Rectangle):
     def __init__(self, size):
         """ init method"""
 
-        super().__init__(size, size)
         self.integer_validator('size', self.__size)
+        super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
