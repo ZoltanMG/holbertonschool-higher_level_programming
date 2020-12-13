12cce97b5b50efbc17ec6140526d7f3d66c201ee
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-12-13 22:47:30 +0000

    [update]: refacture

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""
-script that lists all State objects from
-the database hbtn_0e_6_usa
-"""
+'''
+script that prints the first State object from the database hbtn_0e_6_usa
+'''
 
-if __name__ == '__main__':
-    from model_state import Base, State
-    from sqlalchemy import (create_engine)
-    import sys
-    from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+import sys
+if __name__ == "__main__":
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
 
-    from sqlalchemy.orm import sessionmaker
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    first_state = session.query(State).order_by(State.id)[0]
-    print('{}: {}'.format(first_state.id, first_state.name))
+    result = session.query(State).first()
+    if (result):
+        print("{}: {}".format(result.id, result.name))
+
     session.close()
