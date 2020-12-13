9cd7a14b2d3c6450b5b463b76596338831003168
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-12-13 16:31:25 +0000

    [add]: aql

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
                          passwd=arguments[2],
                          db=arguments[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities")
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities
+    JOIN states ON cities.state_id = states.id""")
     states = cur.fetchall()
     for state in states:
         print(state)
