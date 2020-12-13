b547be1be150d30954ebfe4c22d030f4dcf41a44
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-12-13 22:53:15 +0000

    [update]: valide pep8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,7 +8,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 import sys
 if __name__ == "__main__":
-
+    """
+    cript that prints the first State object from the database hbtn_0e_6_usa
+    """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
