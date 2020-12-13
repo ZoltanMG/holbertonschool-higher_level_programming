d785bc8ea75365a4586e36db6ae2f6bfbc58e2ce
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-12-13 20:46:59 +0000

    [update]: valide edge case

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
     from model_state import Base, State
     from sqlalchemy import (create_engine)
     import sys
+    from sqlalchemy.orm import sessionmaker
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
