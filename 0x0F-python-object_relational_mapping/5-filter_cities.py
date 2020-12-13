df2fbb5910c1754164929a3e103f23e4c14948b0
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-12-13 16:58:25 +0000

    [update]: refacture

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,24 +6,16 @@ write one that is safe from MySQL injections!.
 """
 
 
-if __name__ == '__main__':
-    import MySQLdb
-    import sys
-    arguments = sys.argv
-    db = MySQLdb.connect(host="localhost",
-                         user=arguments[1],
-                         passwd=arguments[2],
-                         db=arguments[3])
+if __name__ == "__main__":
+    db = MySQLdb.connect(port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM cities
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities
     JOIN states ON cities.state_id = states.id
     WHERE states.name = %s""", (arguments[4], ))
-    states = cur.fetchall()
-    for num in range(len(states)):
-        for citie in states[num]:
-            if num < len(states) - 1:
-                print(citie, end=', ')
-            else:
-                print(citie)
+    rows = cur.fetchall()
+    cities = []
+    for row in rows:
+        cities.append(row[1])
+    print(', '.join(cities))
     cur.close()
     db.close()
