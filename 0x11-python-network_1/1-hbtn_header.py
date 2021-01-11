ba7139acd5bae3e6f78d3eb09713fe33a045e751
ZoltanMG <zoltanmoragarcia@gmail.com>
2021-01-11 20:26:19 +0000

    [updated]: add documentation and if name

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-import urllib.request
-from sys import argv
+"""
+Write a Python script that takes in a URL, sends a request to the URL and
+displays the value of the X-Request-Id variable found in
+the header of the response.
+"""
 
-with urllib.request.urlopen(argv[1]) as response:
-    dict_html = response.info()
-    if 'X-Request-Id' in dict_html:
-        print(dict_html['X-Request-Id'])
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        dict_html = response.info()
+        if 'X-Request-Id' in dict_html:
+            print(dict_html['X-Request-Id'])
